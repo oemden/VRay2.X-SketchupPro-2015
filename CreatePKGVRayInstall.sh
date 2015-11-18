d2f1991cceff89b75374c786cddbecc15d53ce3d
oemden <oem@oemden.com>
2015-11-19 00:12:08 +0100

    1st commit working script

---
+++ CreatePKGVRayInstall.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+version="2.00.25539"
+identifier="com.example.int.pkg.installVray.2.X.Sketchup2015"
+
+pkgbuild --root ./root --scripts ./scripts --version "$version" --identifier "$identifier" InstallVray2.xForSketchup2015LicenceServer.pkg
+
